c5e6db592f11349688b8f8b139c5ec84857c2b4c
Luigi Iossa <luigi_iossa@hotmail.com>
2023-02-06 15:21:49 +0000

    settimana 2 giorno 1

---
+++ Settimana2/esercizioG1/assets/js/index.js
@@ -0,0 +1,7 @@
+window.alert('Sto studiando JS');
+alert(9+11);
+alert('Clicca per accedere alla pagina');
+
+document.write('<p>Output scritto da Java script</p>');
+
+document.getElementById('title').innerHTML = 'Iniziamo la nostra pratica';
---
+++ Settimana2/esercizioG1/assets/js/naming.js
@@ -0,0 +1,21 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+var pet_preferito = 'cane'
+console.log('cane');
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var smallPet = 'Criceto';
+console.log(smallPet);
+
+var $biganimal = 'Pantera';
+console.log($biganimal);
+
+var savana_animal = 'Giraffa';
+console.log(savana_animal);
+
+var BigFive = 'Leone scritto da Javascript';
+document.write(BigFive);
+
+//var 12 = 'numero';
