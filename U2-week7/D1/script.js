1ccb757317906a8049434145ede71f02e9329b08
Luigi Iossa <luigi_iossa@hotmail.com>
2023-03-14 15:09:11 +0000

    D1

--- U2-week7/D1/script.js
+++ U2-week7/D1/script.js
@@ -1,73 +1,76 @@
-console.log('hello')
-
 class User {
     constructor (firstName, lastName, age, location) {
-        this.name = firstName
-        this.surname = lastName
+        this.firstName = firstName
+        this.lastName = lastName
         this.age = age
         this.location = location
     }
+    compareAge(anotherUser) {
+        if (this.age < anotherUser.age) {
+          return `${anotherUser.firstName} è più vecchio di ${this.firstName}.`
+        } else if (this.age > anotherUser.age) {
+          return `${anotherUser.firstName} è più giovane di ${this.firstName}.`
+        } else {
+          return `${anotherUser.firstName} è della stessa età di ${this.firstName}.`
+        }
+      }
 }
 
 const User1 = new User ('Luigi, Iossa', '29', 'Dakar')
-console.log('Hello' + ' ' + User1.name)
-
 const User2 = new User ('Mario', 'Rossi', '35', 'Milan')
-console.log('Hello' + ' ' + User2.name)
 
+console.log(User1.compareAge(User12))
 
 
 // ESERCIZIO 2
-let petName = document.getElementById('petName');
-let ownerName = document.getElementById('ownerName');
-let species = document.getElementById('species');
-let breed = document.getElementById('breed');
-let button = document.getElementById('add');
-let petList = document.getElementById('petList');
+let petNameField = document.getElementById('petName-field')
+let ownerNameField = document.getElementById('ownerName-field')
+let speciesField = document.getElementById('species-field')
+let breedField = document.getElementById('breed-field')
 
-let contacts = []
+let petList = document.getElementById('petList')
+let addButton = document.getElementById('add')
 
+let pets = []
 
 class Pet {
-    constructor (petName, ownerName, species, breed) {
-        this.petName = petName
-        this.ownerName = ownerName
-        this.species = species
-        this.breed = breed
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName
+    this.ownerName = ownerName
+    this.species = species
+    this.breed = breed
+  }
+
+  checkSameOwner(anotherPet) {
+    if (this.ownerName === anotherPet.ownerName) {
+      return true
+    } else {
+      return false
     }
+  }
 }
 
-// const Dog = new Pet('Fuffy', 'Luigi', 'dog', 'Labrador')
-// console.log(Dog)
-
-const renderList = () => {
-    petList.innerHTML = ''
-    contacts.forEach((Pet) => {
-      // per ogni contatto cosa farò?
-      // 1) creerò un <li> con il suo nome + cognome
-      const newLi = document.createElement('li')
-      // ho ottenuto un <li></li>
-      // ora aggiungo nome + cognome
-      newLi.innerText = Pet.petName +' ' + 'è il' + Pet.species + 'di' + ' ' + Pet.ownerName + ' ' + 'è un' + Pet.breed
-      // ho ottenuto un <li>Gino Paoli</li>
-      // 2) lo aggiungerò all'<ul>
-      petList.appendChild(newLi)
-      // <ul><li>Gino Paoli</li></ul>
-    })
-  }
-  
-  
-  button.addEventListener('click', () => {
-    console.log('bottone cliccato!')
-    let newContact = new Pet(
-        petName.value, // valore dell'input name
-        ownerName.value // valore dell'input surname
-    )
-
-    contacts.push(newContact)
-
-    //   azzera gli input per facilitare l'inserimento di un nuovo contact
-    petName.value = ''
-    ownerName.value = ''
-    renderList()
+const renderList = function () {
+  petList.innerHTML = '' // <-- svuota la lista all'inizio
+  pets.forEach((pet) => {
+    const newLi = document.createElement('li')
+    newLi.innerText =
+      'Nome: ' + pet.petName + ', proprietario: ' + pet.ownerName
+    petList.appendChild(newLi)
   })
+}
+
+addButton.onclick = function () {
+  let newPet = new Pet(
+    petNameField.value,
+    ownerNameField.value,
+    speciesField.value,
+    breedField.value
+  )
+  pets.push(newPet)
+  renderList()
+  petNameField.value = '' // <-- svuota l'input field alla fine
+  ownerNameField.value = '' // <-- svuota l'input field alla fine
+  speciesField.value = '' // <-- svuota l'input field alla fine
+  breedField.value = '' // <-- svuota l'input field alla fine
+}
